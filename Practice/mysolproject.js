bea6a9bb99a50eb440f000febbfa7478848f8151
HRISHI KUMAWAT <102140797+hrishi008@users.noreply.github.com>
2022-04-30 13:16:28 +0530

    Add files via upload

--- Practice/mysolproject.js
+++ Practice/mysolproject.js
@@ -1,1 +1,46 @@
+alert("welcome..lets have some questions");
+var fn= prompt("whats your first name");
+var ln= prompt("whats your last name");
+var age= prompt("whats your age");
+var height= prompt("whats your height in cm");
+var pn= prompt("whats your pet name");
+var fna= fn[0];
+var lna= ln[0];
 
+if (fna=lna)
+{
+    if(age < 30 && age > 20)
+    {
+        if(height>=170)
+        {
+            for (let index = 0; index < pn.length; index++) 
+            {
+                
+                if (pn[index]==="y") {
+                    console.log("sucess");
+                    break;
+                }else{
+                    console.log("fail");
+                
+                }
+                
+                 
+                
+            }
+
+        }else{
+            console.log("fail");
+        
+        }
+        
+
+    }else{
+        console.log("fail");
+    
+    }
+    
+}
+else{
+    console.log("fail");
+
+}
